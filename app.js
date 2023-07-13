7da5c6f5cc02c46c92c0e6cccf9ce5ed01e01681
Andhika Herdha Jati Samudra <101403750+klluv@users.noreply.github.com>
2023-07-13 13:33:49 +0700

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 new Vue ({
     el:'#vue-app',
     data: {
-        nama: 'kaka',
-        jurusan: 'sija',
-        situs: 'https://www.youtube.com',
-        situsTag: '<a href="www.google.com">situs google</a>'
-    },
-    methods: {
-        title: function(lokasi){
-            return 'ainosi ' + lokasi +' ' + this.nama;
-        }
+        error: false,
+        success: false 
     }
-});
+})
