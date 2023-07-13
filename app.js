81e5a84534087e80a508d447ace856c4e60aa07d
Andhika Herdha Jati Samudra <101403750+klluv@users.noreply.github.com>
2023-07-13 10:15:43 +0700

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 new Vue ({
     el:'#vue-app',
     data: {
-        nama: 'kaka',
-        jurusan: 'sija',
-        situs: 'https://www.youtube.com',
-        situsTag: '<a href="www.google.com">situs google</a>'
+        age: 10,
+        a: 0,
+        b: 0
     },
+    //dengan method, console log akan berjalan semuanya (dua button akan run)
     methods: {
-        title: function(lokasi){
-            return 'ainosi ' + lokasi +' ' + this.nama;
-        }
+       /*ageMinA: function() {
+        console.log('ageMinA');
+        return this.age - this.a;  
+       },
+       ageMinB: function() {
+        console.log(ageMinB);
+        return this.age - this.b;
+       }*/
+    },
+
+    //dengan computed, console log hanya akan berjalan satu button. Maka, csistem akan berjalan lebih efektif
+    computed: {
+        ageMinA: function() {
+            console.log('ageMinA');
+            return this.age - this.a;  
+           },
+           ageMinB: function() {
+            console.log('ageMinB');
+            return this.age - this.b;
+           }
     }
 });
