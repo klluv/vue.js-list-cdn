ed0c124bb91bc9a043e3ccdfa211c9bf15953925
Andhika Herdha Jati Samudra <101403750+klluv@users.noreply.github.com>
2023-07-18 14:04:40 +0700

    Tests.vue

---
+++ style.css
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="test in tests">{{ test }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+          tests: ['test1', 'test2', 'test3']
+        }
+    }
+}
+</script>
+
+<style>
+</style>
